c3fce56d44ed8e79354693d7c2d826c0c7252800
Lev1ossa <ikehhui@gmail.com>
2023-05-01 04:51:43 +0600

    feat: add keys object for DOM generating purpose

---
+++ app.js
@@ -0,0 +1,775 @@
+const keys = {
+  backquote: {
+    eventCode: 'Backquote',
+    keysRow: 1,
+    ru: {
+      lowerCase: 'ё',
+      upperCase: 'Ё',
+    },
+    en: {
+      lowerCase: '\'',
+      upperCase: '~',
+    },
+  },
+  digit1: {
+    eventCode: 'Digit1',
+    keysRow: 1,
+    ru: {
+      lowerCase: '1',
+      upperCase: '!',
+    },
+    en: {
+      lowerCase: '1',
+      upperCase: '!',
+    },
+  },
+  digit2: {
+    eventCode: 'Digit2',
+    keysRow: 1,
+    ru: {
+      lowerCase: '2',
+      upperCase: '"',
+    },
+    en: {
+      lowerCase: '2',
+      upperCase: '@',
+    },
+  },
+  digit3: {
+    eventCode: 'Digit3',
+    keysRow: 1,
+    ru: {
+      lowerCase: '3',
+      upperCase: '№',
+    },
+    en: {
+      lowerCase: '3',
+      upperCase: '#',
+    },
+  },
+  digit4: {
+    eventCode: 'Digit4',
+    keysRow: 1,
+    ru: {
+      lowerCase: '4',
+      upperCase: ';',
+    },
+    en: {
+      lowerCase: '4',
+      upperCase: '$',
+    },
+  },
+  digit5: {
+    eventCode: 'Digit5',
+    keysRow: 1,
+    ru: {
+      lowerCase: '5',
+      upperCase: '%',
+    },
+    en: {
+      lowerCase: '5',
+      upperCase: '%',
+    },
+  },
+  digit6: {
+    eventCode: 'Digit6',
+    keysRow: 1,
+    ru: {
+      lowerCase: '6',
+      upperCase: ':',
+    },
+    en: {
+      lowerCase: '6',
+      upperCase: '^',
+    },
+  },
+  digit7: {
+    eventCode: 'Digit7',
+    keysRow: 1,
+    ru: {
+      lowerCase: '7',
+      upperCase: '?',
+    },
+    en: {
+      lowerCase: '7',
+      upperCase: '&',
+    },
+  },
+  digit8: {
+    eventCode: 'Digit8',
+    keysRow: 1,
+    ru: {
+      lowerCase: '8',
+      upperCase: '*',
+    },
+    en: {
+      lowerCase: '8',
+      upperCase: '*',
+    },
+  },
+  digit9: {
+    eventCode: 'Digit9',
+    keysRow: 1,
+    ru: {
+      lowerCase: '9',
+      upperCase: '(',
+    },
+    en: {
+      lowerCase: '9',
+      upperCase: '(',
+    },
+  },
+  digit0: {
+    eventCode: 'Digit0',
+    keysRow: 1,
+    ru: {
+      lowerCase: '0',
+      upperCase: ')',
+    },
+    en: {
+      lowerCase: '0',
+      upperCase: ')',
+    },
+  },
+  minus: {
+    eventCode: 'Minus',
+    keysRow: 1,
+    ru: {
+      lowerCase: '-',
+      upperCase: '_',
+    },
+    en: {
+      lowerCase: '-',
+      upperCase: '_',
+    },
+  },
+  equal: {
+    eventCode: 'Equal',
+    keysRow: 1,
+    ru: {
+      lowerCase: '=',
+      upperCase: '+',
+    },
+    en: {
+      lowerCase: '=',
+      upperCase: '+',
+    },
+  },
+  backspace: {
+    eventCode: 'Backspace',
+    keysRow: 1,
+    ru: {
+      lowerCase: 'Backspace',
+      upperCase: 'Backspace',
+    },
+    en: {
+      lowerCase: 'Backspace',
+      upperCase: 'Backspace',
+    },
+  },
+  tab: {
+    eventCode: 'Tab',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'Tab',
+      upperCase: 'Tab',
+    },
+    en: {
+      lowerCase: 'Tab',
+      upperCase: 'Tab',
+    },
+  },
+  q: {
+    eventCode: 'KeyQ',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'й',
+      upperCase: 'Й',
+    },
+    en: {
+      lowerCase: 'q',
+      upperCase: 'Q',
+    },
+  },
+  w: {
+    eventCode: 'KeyW',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'ц',
+      upperCase: 'Ц',
+    },
+    en: {
+      lowerCase: 'w',
+      upperCase: 'W',
+    },
+  },
+  e: {
+    eventCode: 'KeyE',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'y',
+      upperCase: 'У',
+    },
+    en: {
+      lowerCase: 'e',
+      upperCase: 'E',
+    },
+  },
+  r: {
+    eventCode: 'KeyR',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'к',
+      upperCase: 'К',
+    },
+    en: {
+      lowerCase: 'r',
+      upperCase: 'R',
+    },
+  },
+  t: {
+    eventCode: 'KeyT',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'е',
+      upperCase: 'Е',
+    },
+    en: {
+      lowerCase: 't',
+      upperCase: 'T',
+    },
+  },
+  y: {
+    eventCode: 'KeyY',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'н',
+      upperCase: 'Н',
+    },
+    en: {
+      lowerCase: 'y',
+      upperCase: 'Y',
+    },
+  },
+  u: {
+    eventCode: 'KeyU',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'г',
+      upperCase: 'Г',
+    },
+    en: {
+      lowerCase: 'u',
+      upperCase: 'U',
+    },
+  },
+  i: {
+    eventCode: 'KeyI',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'ш',
+      upperCase: 'Ш',
+    },
+    en: {
+      lowerCase: 'i',
+      upperCase: 'I',
+    },
+  },
+  o: {
+    eventCode: 'KeyO',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'щ',
+      upperCase: 'Щ',
+    },
+    en: {
+      lowerCase: 'o',
+      upperCase: 'O',
+    },
+  },
+  p: {
+    eventCode: 'KeyP',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'з',
+      upperCase: 'З',
+    },
+    en: {
+      lowerCase: 'p',
+      upperCase: 'P',
+    },
+  },
+  bracketleft: {
+    eventCode: 'BracketLeft',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'x',
+      upperCase: 'X',
+    },
+    en: {
+      lowerCase: '[',
+      upperCase: '{',
+    },
+  },
+  bracketright: {
+    eventCode: 'BracketRight',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'ъ',
+      upperCase: 'Ъ',
+    },
+    en: {
+      lowerCase: ']',
+      upperCase: '}',
+    },
+  },
+  backslash: {
+    eventCode: 'Backslash',
+    keysRow: 2,
+    ru: {
+      lowerCase: '\\',
+      upperCase: '/',
+    },
+    en: {
+      lowerCase: '\\',
+      upperCase: '|',
+    },
+  },
+  delete: {
+    eventCode: 'Delete',
+    keysRow: 2,
+    ru: {
+      lowerCase: 'Del',
+      upperCase: 'Del',
+    },
+    en: {
+      lowerCase: 'Del',
+      upperCase: 'Del',
+    },
+  },
+  capsLock: {
+    eventCode: 'CapsLock',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'CapsLock',
+      upperCase: 'CapsLock',
+    },
+    en: {
+      lowerCase: 'CapsLock',
+      upperCase: 'CapsLock',
+    },
+  },
+  a: {
+    eventCode: 'KeyA',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'ф',
+      upperCase: 'Ф',
+    },
+    en: {
+      lowerCase: 'a',
+      upperCase: 'A',
+    },
+  },
+  s: {
+    eventCode: 'KeyS',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'ы',
+      upperCase: 'Ы',
+    },
+    en: {
+      lowerCase: 's',
+      upperCase: 'S',
+    },
+  },
+  d: {
+    eventCode: 'KeyD',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'в',
+      upperCase: 'В',
+    },
+    en: {
+      lowerCase: 'd',
+      upperCase: 'D',
+    },
+  },
+  f: {
+    eventCode: 'KeyF',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'а',
+      upperCase: 'А',
+    },
+    en: {
+      lowerCase: 'f',
+      upperCase: 'F',
+    },
+  },
+  g: {
+    eventCode: 'KeyG',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'п',
+      upperCase: 'П',
+    },
+    en: {
+      lowerCase: 'g',
+      upperCase: 'G',
+    },
+  },
+  h: {
+    eventCode: 'KeyH',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'р',
+      upperCase: 'Р',
+    },
+    en: {
+      lowerCase: 'h',
+      upperCase: 'H',
+    },
+  },
+  j: {
+    eventCode: 'KeyJ',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'о',
+      upperCase: 'О',
+    },
+    en: {
+      lowerCase: 'j',
+      upperCase: 'J',
+    },
+  },
+  k: {
+    eventCode: 'KeyK',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'л',
+      upperCase: 'Л',
+    },
+    en: {
+      lowerCase: 'k',
+      upperCase: 'K',
+    },
+  },
+  l: {
+    eventCode: 'KeyL',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'д',
+      upperCase: 'Д',
+    },
+    en: {
+      lowerCase: 'l',
+      upperCase: 'L',
+    },
+  },
+  semicolon: {
+    eventCode: 'Semicolon',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'ж',
+      upperCase: 'Ж',
+    },
+    en: {
+      lowerCase: ';',
+      upperCase: ':',
+    },
+  },
+  quote: {
+    eventCode: 'Quote',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'э',
+      upperCase: 'Э',
+    },
+    en: {
+      lowerCase: '\'',
+      upperCase: '"',
+    },
+  },
+  enter: {
+    eventCode: 'Enter',
+    keysRow: 3,
+    ru: {
+      lowerCase: 'Enter',
+      upperCase: 'Enter',
+    },
+    en: {
+      lowerCase: 'Enter',
+      upperCase: 'Enter',
+    },
+  },
+  shiftLeft: {
+    eventCode: 'ShiftLeft',
+    keysRow: 4,
+    ru: {
+      lowerCase: 'Shift',
+      upperCase: 'Shift',
+    },
+    en: {
+      lowerCase: 'Shift',
+      upperCase: 'Shift',
+    },
+  },
+  z: {
+    eventCode: 'KeyZ',
+    keysRow: 4,
+    ru: {
+      lowerCase: 'я',
+      upperCase: 'Я',
+    },
+    en: {
+      lowerCase: 'z',
+      upperCase: 'Z',
+    },
+  },
+  x: {
+    eventCode: 'KeyX',
+    keysRow: 4,
+    ru: {
+      lowerCase: 'ч',
+      upperCase: 'Ч',
+    },
+    en: {
+      lowerCase: 'x',
+      upperCase: 'X',
+    },
+  },
+  c: {
+    eventCode: 'KeyC',
+    keysRow: 4,
+    ru: {
+      lowerCase: 'с',
+      upperCase: 'С',
+    },
+    en: {
+      lowerCase: 'c',
+      upperCase: 'C',
+    },
+  },
+  v: {
+    eventCode: 'KeyV',
+    keysRow: 4,
+    ru: {
+      lowerCase: 'м',
+      upperCase: 'М',
+    },
+    en: {
+      lowerCase: 'v',
+      upperCase: 'V',
+    },
+  },
+  b: {
+    eventCode: 'KeyB',
+    keysRow: 4,
+    ru: {
+      lowerCase: 'и',
+      upperCase: 'И',
+    },
+    en: {
+      lowerCase: 'b',
+      upperCase: 'B',
+    },
+  },
+  n: {
+    eventCode: 'KeyN',
+    keysRow: 4,
+    ru: {
+      lowerCase: 'т',
+      upperCase: 'Т',
+    },
+    en: {
+      lowerCase: 'n',
+      upperCase: 'N',
+    },
+  },
+  m: {
+    eventCode: 'KeyM',
+    keysRow: 4,
+    ru: {
+      lowerCase: 'ь',
+      upperCase: 'Ь',
+    },
+    en: {
+      lowerCase: 'm',
+      upperCase: 'M',
+    },
+  },
+  comma: {
+    eventCode: 'Comma',
+    keysRow: 4,
+    ru: {
+      lowerCase: 'б',
+      upperCase: 'Б',
+    },
+    en: {
+      lowerCase: ',',
+      upperCase: '<',
+    },
+  },
+  period: {
+    eventCode: 'Period',
+    keysRow: 4,
+    ru: {
+      lowerCase: 'ю',
+      upperCase: 'Ю',
+    },
+    en: {
+      lowerCase: '.',
+      upperCase: '>',
+    },
+  },
+  slash: {
+    eventCode: 'Slash',
+    keysRow: 4,
+    ru: {
+      lowerCase: '.',
+      upperCase: ',',
+    },
+    en: {
+      lowerCase: '/',
+      upperCase: '?',
+    },
+  },
+  arrowUp: {
+    eventCode: 'ArrowUp',
+    keysRow: 4,
+    ru: {
+      lowerCase: '&#8593;',
+      upperCase: '&#8593;',
+    },
+    en: {
+      lowerCase: '&#8593;',
+      upperCase: '&#8593;',
+    },
+  },
+  shiftRight: {
+    eventCode: 'ShiftRight',
+    keysRow: 4,
+    ru: {
+      lowerCase: 'Shift',
+      upperCase: 'Shift',
+    },
+    en: {
+      lowerCase: 'Shift',
+      upperCase: 'Shift',
+    },
+  },
+  controlLeft: {
+    eventCode: 'ControlLeft',
+    keysRow: 5,
+    ru: {
+      lowerCase: 'Ctrl',
+      upperCase: 'Ctrl',
+    },
+    en: {
+      lowerCase: 'Ctrl',
+      upperCase: 'Ctrl',
+    },
+  },
+  metaLeft: {
+    eventCode: 'MetaLeft',
+    keysRow: 5,
+    ru: {
+      lowerCase: 'Win',
+      upperCase: 'Win',
+    },
+    en: {
+      lowerCase: 'Win',
+      upperCase: 'Win',
+    },
+  },
+  altLeft: {
+    eventCode: 'AltLeft',
+    keysRow: 5,
+    ru: {
+      lowerCase: 'Alt',
+      upperCase: 'Alt',
+    },
+    en: {
+      lowerCase: 'Alt',
+      upperCase: 'Alt',
+    },
+  },
+  space: {
+    eventCode: 'Space',
+    keysRow: 5,
+    ru: {
+      lowerCase: ' ',
+      upperCase: ' ',
+    },
+    en: {
+      lowerCase: ' ',
+      upperCase: ' ',
+    },
+  },
+  altRight: {
+    eventCode: 'AltRight',
+    keysRow: 5,
+    ru: {
+      lowerCase: 'Alt',
+      upperCase: 'Alt',
+    },
+    en: {
+      lowerCase: 'Alt',
+      upperCase: 'Alt',
+    },
+  },
+  arrowLeft: {
+    eventCode: 'ArrowLeft',
+    keysRow: 5,
+    ru: {
+      lowerCase: '&#8592;',
+      upperCase: '&#8592;',
+    },
+    en: {
+      lowerCase: '&#8592;',
+      upperCase: '&#8592;',
+    },
+  },
+  arrowDown: {
+    eventCode: 'ArrowDown',
+    keysRow: 5,
+    ru: {
+      lowerCase: '&#8595;',
+      upperCase: '&#8595;',
+    },
+    en: {
+      lowerCase: '&#8595;',
+      upperCase: '&#8595;',
+    },
+  },
+  arrowRight: {
+    eventCode: 'ArrowRight',
+    keysRow: 5,
+    ru: {
+      lowerCase: '&#8594;',
+      upperCase: '&#8594;',
+    },
+    en: {
+      lowerCase: '&#8594;',
+      upperCase: '&#8594;',
+    },
+  },
+  controlRight: {
+    eventCode: 'ControlRight',
+    keysRow: 5,
+    ru: {
+      lowerCase: 'Ctrl',
+      upperCase: 'Ctrl',
+    },
+    en: {
+      lowerCase: 'Ctrl',
+      upperCase: 'Ctrl',
+    },
+  },
+};
+
+// TODO delete this
+if (keys) {
+  console.log('delete me!');
+}
